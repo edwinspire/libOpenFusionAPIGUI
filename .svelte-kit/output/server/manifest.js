f2533f5bcf73820ebbd1a6d348c820ba79aec746
Edwin De La Cruz <edwinspire@gmail.com>
2024-01-17 20:27:53 -0500

    reBuild

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.-BDFkw9h.js","app":"_app/immutable/entry/app.C8gDe2El.js","imports":["_app/immutable/entry/start.-BDFkw9h.js","_app/immutable/chunks/entry.gn-DgmwB.js","_app/immutable/chunks/scheduler.eyujEKWe.js","_app/immutable/chunks/index.i9oGv7sS.js","_app/immutable/entry/app.C8gDe2El.js","_app/immutable/chunks/scheduler.eyujEKWe.js","_app/immutable/chunks/index.ztFHNGF1.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.NZWPpdV8.js","app":"_app/immutable/entry/app.MQX_dCVQ.js","imports":["_app/immutable/entry/start.NZWPpdV8.js","_app/immutable/chunks/entry.6M97Ehu0.js","_app/immutable/chunks/scheduler.eyujEKWe.js","_app/immutable/chunks/index.i9oGv7sS.js","_app/immutable/entry/app.MQX_dCVQ.js","_app/immutable/chunks/scheduler.eyujEKWe.js","_app/immutable/chunks/index.ztFHNGF1.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,6 @@
+// Reexport your entry components here
+import 'bulma/css/bulma.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import OpenFusionAPIGUI from './OpenFusionAPI/index.svelte';
 
 export {
