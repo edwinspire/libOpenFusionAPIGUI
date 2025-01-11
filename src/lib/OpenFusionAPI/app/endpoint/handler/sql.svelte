0633732220c10f48792530f8ca5ad1802b99ede9
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edwindelacruz@Farmaenlace.com>
2025-01-10 20:19:40 -0500

    Upgrade Svelte 5 - sql

--- src/lib/OpenFusionAPI/app/endpoint/handler/sql.svelte
+++ src/lib/OpenFusionAPI/app/endpoint/handler/sql.svelte
@@ -1,24 +1,16 @@
 <script>
-	// @ts-nocheck
-	import { onMount } from 'svelte';
-	import { Tab, EditorCode, RESTTester, JSONView, BasicSelect } from '@edwinspire/svelte-components';
+	import { onMount, onDestroy } from 'svelte';
+	import { Tab, EditorCode, RESTTester, JSONView } from '@edwinspire/svelte-components';
 	import AppVars from '../../app_vars.svelte';
-	import Endpoint from '../handler/endpoint.svelte';
-	//import ParamsVarJSON from '../widgets/parameters_var_json.svelte';
+
+	let { row = $bindable({ endpoint: '', method: '', environment: '' }), onchange = () => {} } =
+		$props();
+
+	let use_var_cnx = $state(false);
+	let cnx_param_json = $state({});
+	let cnx_param_var = $state('');
 
 	/**
-	 * @type {any}
-	 */
-	//let code;
-	export let row = { endpoint: '', method: '', environment: '' };
-
-	//let sample_bind_post_string = '{}';
-	let fnApiTester;
-	let use_var_cnx = false;
-	let cnx_param_json = {};
-	let cnx_param_var = '';
-
-/**
  SELECT = 'SELECT',
   INSERT = 'INSERT',
   UPDATE = 'UPDATE',
@@ -31,107 +23,43 @@
   SHOWCONSTRAINTS = 'SHOWCONSTRAINTS',
   */
 
-	let QueryTypes = [
-		{
-			id: 'SELECT',
-			value: 'SELECT',
-			enabled: true,
-			description: 'SELECT'
-		},
-		{
-			id: 'INSERT',
-			value: 'INSERT',
-			enabled: true,
-			description: 'INSERT'
-		},
-		{
-			id: 'UPDATE',
-			value: 'UPDATE',
-			enabled: true,
-			description: 'UPDATE'
-		},
-		{
-			id: 'BULKUPDATE',
-			value: 'BULKUPDATE',
-			enabled: false,
-			description: 'BULKUPDATE'
-		},
-		{
-			id: 'DELETE',
-			value: 'DELETE',
-			enabled: true,
-			description: 'DELETE'
-		},
-		{
-			id: 'UPSERT',
-			value: 'UPSERT',
-			enabled: false,
-			description: 'UPSERT'
-		},
-		{
-			id: 'SHOWINDEXES',
-			value: 'SHOWINDEXES',
-			enabled: false,
-			description: 'SHOWINDEXES'
-		},
-		{
-			id: 'DESCRIBE',
-			value: 'DESCRIBE',
-			enabled: false,
-			description: 'DESCRIBE'
-		},
-		{
-			id: 'RAW',
-			value: 'RAW',
-			enabled: true,
-			description: 'RAW'
-		},
-		{
-			id: 'SHOWCONSTRAINTS',
-			value: 'SHOWCONSTRAINTS',
-			enabled: false,
-			description: 'SHOWCONSTRAINTS'
-		}
-	];
-
-	let sample_bind_post = {
+	let sample_bind_post = $state({
 		bind: {
 			your_param_01: '0002081614',
 			your_param_02: 123,
 			your_param_03: 'ABC'
 		}
-	};
+	});
 
-	let sample_replace_post = {
+	let sample_replace_post = $state({
 		replacements: {
 			your_param_01: ['0002081614', '986535OKE']
 		}
-	};
+	});
 
-	let tabList = [
-		{ label: 'Query', isActive: true, classIcon: ' fa-solid fa-database ' },
-		{ label: 'Pass Parameters' },
-		{ label: 'Connection Parameters' },
-		{ label: 'App Variables' },
-		{ label: 'Tester' }
-	];
+	let tabList = $state([
+		{ label: 'Query', isActive: true, classIcon: ' fa-solid fa-database ', component: tab_query },
+		{ label: 'Pass Parameters', component: tab_pass_params },
+		{ label: 'Connection Parameters', component: tab_cnx_params },
+		{ label: 'App Variables', component: tab_app_vars },
+		{ label: 'Tester', component: tab_tester }
+	]);
 
-	let query_code = 'SELECT 1+1;';
-	let internal_data_test;
+	let query_code = $state('SELECT 1+1;');
 
-	$: row.code, ParseCode();
-	$: row.data_test, setDataTest();
+	let timeoutChange;
 
-	function setDataTest() {
-		internal_data_test = { ...row.data_test };
-		//console.log('internal_data_test', internal_data_test);
-	}
+	$inspect(row.code).with((type) => {
+		//console.log('row.code >>>>>>>>>>>>> ', type, row);
+		if (type === 'update' || type === 'init') {
+			clearTimeout(parseCode());
+			timeoutChange = setTimeout(() => {
+				parseCode();
+			}, 750);
+		}
+	});
 
-	export function reset() {
-		ParseCode();
-	}
-
-	function ParseCode() {
+	function parseCode() {
 		try {
 			let params = JSON.parse(row.code || '{}');
 
@@ -140,7 +68,7 @@
 			}
 
 			if (params && params.config) {
-				//console.log('>>> ParseCode >> ', typeof params.config, params.config);
+				//console.log('>>> parseCode >> ', typeof params.config, params.config);
 
 				if (typeof params.config === 'object') {
 					cnx_param_json = params.config;
@@ -158,8 +86,12 @@
 		}
 	}
 
-	export function getData() {
-		let data = { code: getCode(), data_test: internal_data_test };
+	function fnOnChange() {
+		onchange(getData());
+	}
+
+	function getData() {
+		let data = { code: getCode(), data_test: $state.snapshot(row.data_test) };
 		//	console.log('> getData > SQL', data);
 		return data;
 	}
@@ -194,163 +126,168 @@
 
 	onMount(() => {
 		//	console.log(code);
-		ParseCode();
+		parseCode();
 		//	sample_bind_post_string = JSON.stringify(sample_bind_post);
+	});
+	onDestroy(() => {
+		clearTimeout(timeoutChange);
 	});
 </script>
 
-<Tab bind:tabs={tabList}>
-	<div class={tabList[0].isActive ? '' : 'is-hidden'}>
-		<div>
-			<div>
-				<div class="content is-small">
-					The parameters must have a name like <span style="font-style: oblique; font-weight: bold;"
-						>$nameparameter</span
-					>
-					to bind, or <span style="font-style: oblique; font-weight: bold;">:nameparameter</span> to
-					replacements. The values ​​you send in the request. For more information you can consult
-					the
-					<a href="https://sequelize.org/docs/v6/core-concepts/raw-queries/#bind-parameter"
-						>sequelize</a
-					>
-					documentation.
-				</div>
-			</div>
-		</div>
-		<WarnPrd bind:environment={row.environment}></WarnPrd>
-
-
-		<EditorCode isReadOnly={false} title={'Query to be executed'} lang="sql" bind:code={query_code}
-		></EditorCode>
-	</div>
-
-	<div class={tabList[1].isActive ? '' : 'is-hidden'}>
-		<div class="content is-small">
-			<p>
-				In sequelize you can pass parameters using <strong>"bind"</strong> or
-				<strong>"replacements"</strong>
-				depending on the case. You can refer to
-				<a href="https://sequelize.org/docs/v6/core-concepts/raw-queries/#bind-parameter"
-					>Sequelize Parameters</a
-				> for more details.
-			</p>
-
-			{#if row.method === 'GET'}
-				<div class="block">
-					<div class="icon-text">
-						<span class="icon has-text-warning">
-							<i class="fas fa-exclamation-triangle"></i>
-						</span>
-						<span>GET Method - Warning</span>
-					</div>
-
-					<p class="block">
-						The GET method is recommended only for simple requests where there is no input
-						parameters, or failing that the parameters are key-value on query request, which will be
-						used to join with the variables of the SQL query.
-					</p>
-
-					<div class="block">
-						If you send a parameter that is not present in the query you will get an error similar
-						to: <code>Column index out of range</code>
-					</div>
-				</div>
-			{:else if row.method === 'POST'}
-				<div class="block">
-					<div class="icon-text">
-						<span class="icon has-text-info">
-							<i class="fa-solid fa-circle-exclamation"></i>
-						</span>
-						<span
-							>When you use the POST method, the input parameters must be sent in the BODY in JSON
-							format, using the following example:</span
-						>
-					</div>
-					<br />
-
-					<JSONView bind:jsonObject={sample_bind_post} />
-
-					<br />
-
-					<div class="icon-text">
-						<span class="icon has-text-info">
-							<i class="fa-solid fa-circle-exclamation"></i>
-						</span>
-						<span>You can also use "replacements" used by sequelize., for example:</span>
-					</div>
-					<br />
-
-					<JSONView bind:jsonObject={sample_replace_post} />
-				</div>
-
-				<div class="icon-text">
-					<span class="icon has-text-warning">
-						<i class="fa-solid fa-circle-exclamation"></i>
-					</span>
-					<span>Only "bind" or "replacements" can be used at the same time.</span>
-				</div>
-			{/if}
-		</div>
-	</div>
-
-	<div class={tabList[2].isActive ? '' : 'is-hidden'}>
+{#snippet tab_query()}
+	<div>
 		<div>
 			<div class="content is-small">
-				Configuration parameters used by sequelize, visit <a href="https://sequelize.org/"
-					>https://sequelize.org/</a
+				The parameters must have a name like <span style="font-style: oblique; font-weight: bold;"
+					>$nameparameter</span
 				>
-				for more information.
-				<br />
-				You can also use the name of an application variable to use it, for example
-				<strong>$_VAR_NAME</strong>.
+				to bind, or <span style="font-style: oblique; font-weight: bold;">:nameparameter</span> to
+				replacements. The values ​​you send in the request. For more information you can consult the
+				<a href="https://sequelize.org/docs/v6/core-concepts/raw-queries/#bind-parameter"
+					>sequelize</a
+				>
+				documentation.
 			</div>
 		</div>
+	</div>
 
-		<div class="box">
-			<div class="buttons has-addons">
-				<button
-					class="button is-small {use_var_cnx ? '' : 'is-active is-primary'}"
-					on:click={() => {
-						use_var_cnx = false;
-					}}>JSON Parameters</button
-				>
-				<button
-					class="button is-small {use_var_cnx ? 'is-active is-primary' : ''}"
-					on:click={() => {
-						use_var_cnx = true;
-					}}>Use Variable</button
-				>
-			</div>
+	<EditorCode isReadOnly={false} title={'Query to be executed'} lang="sql" bind:code={query_code}
+	></EditorCode>
+{/snippet}
 
-			{#if !use_var_cnx}
-				<EditorCode isReadOnly={false} lang="json" bind:code={cnx_param_json}></EditorCode>
-			{:else}
-				<div class="control has-icons-left">
-					<input
-						class="input is-small"
-						type="text"
-						placeholder="$_VAR_NAME"
-						bind:value={cnx_param_var}
-					/>
-					<span class="icon is-small is-left">
-						<i class="fa-regular fa-keyboard"></i>
+{#snippet tab_pass_params()}
+	<div class="content is-small">
+		<p>
+			In sequelize you can pass parameters using <strong>"bind"</strong> or
+			<strong>"replacements"</strong>
+			depending on the case. You can refer to
+			<a href="https://sequelize.org/docs/v6/core-concepts/raw-queries/#bind-parameter"
+				>Sequelize Parameters</a
+			> for more details.
+		</p>
+
+		{#if row.method === 'GET'}
+			<div class="block">
+				<div class="icon-text">
+					<span class="icon has-text-warning">
+						<i class="fas fa-exclamation-triangle"></i>
 					</span>
+					<span>GET Method - Warning</span>
 				</div>
-			{/if}
+
+				<p class="block">
+					The GET method is recommended only for simple requests where there is no input parameters,
+					or failing that the parameters are key-value on query request, which will be used to join
+					with the variables of the SQL query.
+				</p>
+
+				<div class="block">
+					If you send a parameter that is not present in the query you will get an error similar to: <code
+						>Column index out of range</code
+					>
+				</div>
+			</div>
+		{:else if row.method === 'POST'}
+			<div class="block">
+				<div class="icon-text">
+					<span class="icon has-text-info">
+						<i class="fa-solid fa-circle-exclamation"></i>
+					</span>
+					<span
+						>When you use the POST method, the input parameters must be sent in the BODY in JSON
+						format, using the following example:</span
+					>
+				</div>
+				<br />
+
+				<JSONView bind:jsonObject={sample_bind_post} />
+
+				<br />
+
+				<div class="icon-text">
+					<span class="icon has-text-info">
+						<i class="fa-solid fa-circle-exclamation"></i>
+					</span>
+					<span>You can also use "replacements" used by sequelize., for example:</span>
+				</div>
+				<br />
+
+				<JSONView bind:jsonObject={sample_replace_post} />
+			</div>
+
+			<div class="icon-text">
+				<span class="icon has-text-warning">
+					<i class="fa-solid fa-circle-exclamation"></i>
+				</span>
+				<span>Only "bind" or "replacements" can be used at the same time.</span>
+			</div>
+		{/if}
+	</div>
+{/snippet}
+
+{#snippet tab_cnx_params()}
+	<div>
+		<div class="content is-small">
+			Configuration parameters used by sequelize, visit <a href="https://sequelize.org/"
+				>https://sequelize.org/</a
+			>
+			for more information.
+			<br />
+			You can also use the name of an application variable to use it, for example
+			<strong>$_VAR_NAME</strong>.
 		</div>
 	</div>
 
-	<div class={tabList[3].isActive ? '' : 'is-hidden'}>
-		<AppVars environment={row.environment} isReadOnly={true}></AppVars>
-	</div>
+	<div class="box">
+		<div class="buttons has-addons">
+			<button
+				class="button is-small {use_var_cnx ? '' : 'is-active is-primary'}"
+				onclick={() => {
+					use_var_cnx = false;
+				}}>JSON Parameters</button
+			>
+			<button
+				class="button is-small {use_var_cnx ? 'is-active is-primary' : ''}"
+				onclick={() => {
+					use_var_cnx = true;
+				}}>Use Variable</button
+			>
+		</div>
 
-	<div class={tabList[4].isActive ? '' : 'is-hidden'}>
-				<RESTTester
-			bind:this={fnApiTester}
-			bind:data={internal_data_test}
-			bind:method={row.method}
+		{#if !use_var_cnx}
+			<EditorCode isReadOnly={false} lang="json" bind:code={cnx_param_json}></EditorCode>
+		{:else}
+			<div class="control has-icons-left">
+				<input
+					class="input is-small"
+					type="text"
+					placeholder="$_VAR_NAME"
+					bind:value={cnx_param_var}
+				/>
+				<span class="icon is-small is-left">
+					<i class="fa-regular fa-keyboard"></i>
+				</span>
+			</div>
+		{/if}
+	</div>
+{/snippet}
+
+{#snippet tab_app_vars()}
+	<AppVars environment={row.environment} isReadOnly={true}></AppVars>
+{/snippet}
+
+{#snippet tab_tester()}
+	<div>
+		<RESTTester
+			bind:data={row.data_test}
+			method={row.method}
 			url={row.endpoint}
 			methodDisabled={true}
+			onchange={(c) => {
+				fnOnChange();
+			}}
 		></RESTTester>
 	</div>
-</Tab>
+{/snippet}
+
+<Tab bind:tabs={tabList}></Tab>
