3fc87c9d341af59b651e555cdee542ff527a97f4
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edwindelacruz@Farmaenlace.com>
2025-01-12 00:49:53 -0500

    Upgrade Svelte 5 - icon_label

--- src/lib/OpenFusionAPI/widgets/icon_label.svelte
+++ src/lib/OpenFusionAPI/widgets/icon_label.svelte
@@ -1,12 +1,14 @@
 <script>
-	export let color = '';
-	export let label = ' Label ';
-	export let icon = 'fas fa-home';
+	let {
+		color = $bindable(''),
+		label = $bindable(' Label '),
+		icon = $bindable('fas fa-home')
+	} = $props();
 </script>
 
 <span class="icon-text">
 	<span class="icon has-text-{color}">
-		<i class="{icon}"></i>
+		<i class={icon}></i>
 	</span>
 	<span>{label}</span>
 </span>
