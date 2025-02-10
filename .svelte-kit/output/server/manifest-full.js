e29c8f2a10230c396d537fcad5932d4b4f402fd9
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edwindelacruz@Farmaenlace.com>
2025-02-10 09:41:00 -0500

    Last build

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {start:"_app/immutable/entry/start.D9QpQiob.js",app:"_app/immutable/entry/app.D3z78pbr.js",imports:["_app/immutable/entry/start.D9QpQiob.js","_app/immutable/chunks/ClWki6w0.js","_app/immutable/chunks/C6hcYJ3M.js","_app/immutable/chunks/CiKuGMlB.js","_app/immutable/entry/app.D3z78pbr.js","_app/immutable/chunks/C6hcYJ3M.js","_app/immutable/chunks/Mwwt3995.js","_app/immutable/chunks/CdxaFCRb.js","_app/immutable/chunks/ZCbBkbPR.js","_app/immutable/chunks/CiKuGMlB.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.Ch1SyFk5.js",app:"_app/immutable/entry/app.BWvjm_v0.js",imports:["_app/immutable/entry/start.Ch1SyFk5.js","_app/immutable/chunks/B5NX81uU.js","_app/immutable/chunks/C6hcYJ3M.js","_app/immutable/chunks/CiKuGMlB.js","_app/immutable/entry/app.BWvjm_v0.js","_app/immutable/chunks/C6hcYJ3M.js","_app/immutable/chunks/Mwwt3995.js","_app/immutable/chunks/CdxaFCRb.js","_app/immutable/chunks/ZCbBkbPR.js","_app/immutable/chunks/CiKuGMlB.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
