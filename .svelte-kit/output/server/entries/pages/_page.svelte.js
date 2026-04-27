d8780fd0a375a7db6e259ad1eea1b1d4dc140fc9
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edwindelacruz@Farmaenlace.com>
2026-04-26 21:56:54 -0500

    Rebuild

--- .svelte-kit/output/server/entries/pages/_page.svelte.js
+++ .svelte-kit/output/server/entries/pages/_page.svelte.js
@@ -64,7 +64,7 @@ function Notifications($$renderer, $$props) {
     $$renderer2.push(`<!--]--></div>`);
   });
 }
-const version = "1.3.3";
+const version = "1.3.4";
 function Login($$renderer, $$props) {
   $$renderer.component(($$renderer2) => {
     let username = "";
--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {start:"_app/immutable/entry/start.CNH4lDD6.js",app:"_app/immutable/entry/app.BHBjnau_.js",imports:["_app/immutable/entry/start.CNH4lDD6.js","_app/immutable/chunks/VXlMknEP.js","_app/immutable/chunks/DqasLd8s.js","_app/immutable/chunks/BfOQTNel.js","_app/immutable/entry/app.BHBjnau_.js","_app/immutable/chunks/DqasLd8s.js","_app/immutable/chunks/CQVSmpyi.js","_app/immutable/chunks/NIOxQASV.js","_app/immutable/chunks/BfOQTNel.js","_app/immutable/chunks/k_614q5B.js","_app/immutable/chunks/MNt8RLBV.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.KR269lmC.js",app:"_app/immutable/entry/app.Bu4kb2Wf.js",imports:["_app/immutable/entry/start.KR269lmC.js","_app/immutable/chunks/DbkQFWrW.js","_app/immutable/chunks/DqasLd8s.js","_app/immutable/chunks/BfOQTNel.js","_app/immutable/entry/app.Bu4kb2Wf.js","_app/immutable/chunks/DqasLd8s.js","_app/immutable/chunks/CQVSmpyi.js","_app/immutable/chunks/NIOxQASV.js","_app/immutable/chunks/BfOQTNel.js","_app/immutable/chunks/k_614q5B.js","_app/immutable/chunks/MNt8RLBV.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
